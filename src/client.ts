66127bc4d313a27061d6007323c75fe44eb6c510
Gede Sudimahendra <54178249+deedima3@users.noreply.github.com>
2023-08-24 18:12:21 +0800

    fix/env-import

--- src/client.ts
+++ src/client.ts
@@ -1,12 +1,11 @@
-import { PUBLIC_API_TOKEN, PUBLIC_BASE_API } from '$env/static/public';
 import { HoudiniClient } from '$hoody';
 
 export default new HoudiniClient({
-    url: PUBLIC_BASE_API,
+    url: import.meta.env.PUBLIC_BASE_API,
     fetchParams() {
         return {
             headers: {
-                Authorization: `Bearer ${PUBLIC_API_TOKEN}`,
+                Authorization: `Bearer ${import.meta.env.PUBLIC_API_TOKEN}`,
             }
         }
     }
--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,8 @@ export default defineConfig(({ mode }) => {
 	// Load env file based on `mode` in the current working directory.
 	// Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
 	const env = loadEnv(mode, process.cwd(), '')
+	console.log(env)
 	return {
-		// vite config
 		define: {
 			PUBLIC_BASE_API: JSON.stringify(env.PUBLIC_BASE_API),
 			PUBLIC_API_TOKEN: JSON.stringify(env.PUBLIC_API_TOKEN)
