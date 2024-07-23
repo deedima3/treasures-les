9a8283e52e52bbf63fd1eff950aa1dbf6e9f5ec2
Putu Widyantara <putuwaw973@gmail.com>
2024-07-23 12:49:15 +0800

    fix/env-not-exported

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,26 +1,18 @@
 import { google } from 'googleapis';
-import {
-  GOOGLE_TYPE,
-  GOOGLE_CLIENT_EMAIL,
-  GOOGLE_PRIVATE_KEY,
-  GOOGLE_PRIVATE_KEY_ID,
-  GOOGLE_PROJECT_ID,
-  GOOGLE_UNIVERSE_DOMAIN,
-  GOOGLE_SPREADSHEET_ID,
-  GOOGLE_SPREADSHEET_RANGE,
-} from '$env/static/private';
+import { env } from '$env/dynamic/private';
+
 import { getFormattedDateTime } from '$utils/utils.js';
 
 export async function _insertDataToGsheet(email: string) {
   const auth = await google.auth.getClient({
-    projectId: GOOGLE_PROJECT_ID,
+    projectId: env.VITE_GOOGLE_PROJECT_ID,
     credentials: {
-      "type": GOOGLE_TYPE,
-      "project_id": GOOGLE_PROJECT_ID,
-      "private_key_id": GOOGLE_PRIVATE_KEY_ID,
-      "private_key": GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-      "client_email": GOOGLE_CLIENT_EMAIL,
-      "universe_domain": GOOGLE_UNIVERSE_DOMAIN,
+      "type": env.VITE_GOOGLE_TYPE,
+      "project_id": env.VITE_GOOGLE_PROJECT_ID,
+      "private_key_id": env.VITE_GOOGLE_PRIVATE_KEY_ID,
+      "private_key": env.VITE_GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+      "client_email": env.VITE_GOOGLE_CLIENT_EMAIL,
+      "universe_domain": env.VITE_GOOGLE_UNIVERSE_DOMAIN,
     },
     scopes: ["https://www.googleapis.com/auth/spreadsheets"],
   });
@@ -28,8 +20,8 @@ export async function _insertDataToGsheet(email: string) {
   const gsheets = google.sheets({ version: "v4", auth: auth });
   const response = await gsheets.spreadsheets.values.append({
     auth: auth,
-    spreadsheetId: GOOGLE_SPREADSHEET_ID,
-    range: GOOGLE_SPREADSHEET_RANGE,
+    spreadsheetId: env.VITE_GOOGLE_SPREADSHEET_ID,
+    range: env.VITE_GOOGLE_SPREADSHEET_RANGE,
     valueInputOption: "USER_ENTERED",
     requestBody: {
       values: [
