de83e78240dc57cff94fdab1d93e64276b015c91
Putu Widyantara <putuwaw973@gmail.com>
2024-07-25 23:00:24 +0800

    feat/update-favicon

--- src/components/SEO/Favicon.svelte
+++ src/components/SEO/Favicon.svelte
@@ -1,19 +1,28 @@
 <svelte:head>
-	<link rel="apple-touch-icon" sizes="57x57" href="/favicon/apple-icon-57x57.png" />
-	<link rel="apple-touch-icon" sizes="60x60" href="/favicon/apple-icon-60x60.png" />
-	<link rel="apple-touch-icon" sizes="72x72" href="/favicon/apple-icon-72x72.png" />
-	<link rel="apple-touch-icon" sizes="76x76" href="/favicon/apple-icon-76x76.png" />
-	<link rel="apple-touch-icon" sizes="114x114" href="/favicon/apple-icon-114x114.png" />
-	<link rel="apple-touch-icon" sizes="120x120" href="/favicon/apple-icon-120x120.png" />
-	<link rel="apple-touch-icon" sizes="144x144" href="/favicon/apple-icon-144x144.png" />
-	<link rel="apple-touch-icon" sizes="152x152" href="/favicon/apple-icon-152x152.png" />
-	<link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-icon-180x180.png" />
-	<link rel="icon" type="image/png" sizes="192x192" href="/favicon/android-icon-192x192.png" />
-	<link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
-	<link rel="icon" type="image/png" sizes="96x96" href="/favicon/favicon-96x96.png" />
+	<link rel="shortcut icon" type="image/x-icon" href="/favicon/favicon.ico" />
 	<link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
-	<link rel="manifest" href="/favicon/manifest.json" />
-	<meta name="msapplication-TileColor" content="#ffffff" />
-	<meta name="msapplication-TileImage" content="/favicon/ms-icon-144x144.png" />
-	<meta name="theme-color" content="#ffffff" />
+	<link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+	<link rel="icon" type="image/png" sizes="64x64" href="/favicon/favicon-64x64.png" />
+	<link rel="icon" type="image/png" sizes="96x96" href="/favicon/favicon-96x96.png" />
+	
+	<link rel="apple-touch-icon" sizes="57x57" href="/favicon/apple-57x57-touch-icon.png" />
+	<link rel="apple-touch-icon" sizes="60x60" href="/favicon/apple-60x60-touch-icon.png" />
+	<link rel="apple-touch-icon" sizes="72x72" href="/favicon/apple-72x72-touch-icon.png" />
+	<link rel="apple-touch-icon" sizes="76x76" href="/favicon/apple-76x76-touch-icon.png" />
+	<link rel="apple-touch-icon" sizes="114x114" href="/favicon/apple-114x114-touch-icon.png" />
+	<link rel="apple-touch-icon" sizes="120x120" href="/favicon/apple-120x120-touch-icon.png" />
+	<link rel="apple-touch-icon" sizes="144x144" href="/favicon/apple-144x144-touch-icon.png" />
+	<link rel="apple-touch-icon" sizes="152x152" href="/favicon/apple-152x152-touch-icon.png" />
+	<link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-180x180-touch-icon.png" />
+	
+	<link rel="icon" type="image/png" sizes="196x196" href="/favicon/android-chrome-196x196.png" />
+	
+	<meta name="theme-color" content="#424153" />
+	  
+	<meta name="msapplication-TileColor" content="#424153" />
+	<meta name="msapplication-TileImage" content="/favicon/windows-tile.png">
+	<meta name="msapplication-square70x70logo" content="/favicon/windows-small-tile.png" />
+	<meta name="msapplication-square150x150logo" content="/favicon/windows-medium-tile.png" />
+	<meta name="msapplication-wide310x150logo" content="/favicon/windows-wide-tile.png" />
+	<meta name="msapplication-square310x310logo" content="/favicon/windows-large-tile.png" />
 </svelte:head>
