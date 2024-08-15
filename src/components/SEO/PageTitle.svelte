d21f5f44fa06844ff8d31ab147cfb94788f0123f
Putu Widyantara <putuwaw973@gmail.com>
2024-08-15 22:20:35 +0800

    feat/update-favicon-metadata

--- src/components/SEO/PageTitle.svelte
+++ src/components/SEO/PageTitle.svelte
@@ -1,9 +1,21 @@
 <script lang="ts">
 	export let title: string;
+	export let description = `Discover the enchanting secrets of Les Village. Explore breathtaking destinations, unique local products, and unforgettable tour experiences. Plan your perfect getaway today!`;
+	export let image = 'https://visitles.id/les_village.jpg';
+
+	let url = 'https://visitles.id';
+	if (typeof window !== 'undefined') {
+		url = window.location.href;
+	}
 </script>
 
 <svelte:head>
-	<title>
-		Les | {title}
-	</title>
+	<title>Les | {title}</title>
+	<meta name="description" content={description} />
+	<meta name="keywords" content="Visit, Les, Village, Bali, KKN, ADWI" />
+	<meta name="author" content="KKN Desa Les" />
+	<meta property="og:title" content={title} />
+	<meta property="og:description" content={description} />
+	<meta property="og:image" content={image} />
+	<meta property="og:url" content={url} />
 </svelte:head>
